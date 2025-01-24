c22a94c3dde363e211109c344170706224fb6a85
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2025-01-24 05:39:14 +0100

    A script that updates the super admin record

--- models/create_super_admin.py
+++ models/create_super_admin.py
@@ -1,11 +1,15 @@
 """Creates a super admin in the database."""
 import sys
 from werkzeug.security import generate_password_hash
-from models.db import admin_collection
-password = sys.argv[0]
+from db import admin_collection
+if len(sys.argv) < 2 or len(sys.argv) > 2:
+    print('Usage: python create_super_admin.py <password>')
+    sys.exit(1)
+password = sys.argv[1]
 if len(password) > 15:
     print('password should not be more than 15 characters')
     sys.exit(1)
 hashed_pw = generate_password_hash(password)
-updated = admin_collection.updateOne({'firstName': 'Daniel'}, {'password': 'hashed_pw'})
-if (updated and)
+updated = admin_collection.update_one({'firstName': 'Daniel'}, {'$set': {'password': hashed_pw}})
+if updated and updated.modified_count > 0:
+    print('super admin updated: password updated')
